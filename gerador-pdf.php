9fbd87608105e6548b53d447c6fa8f0e2aa9fc2e
Felipe Melo <feelipe.ooliveiraa@gmail.com>
2025-05-13 22:02:24 -0300

    Adicionando a funcionalidade Gerar relatorio em PDF

--- gerador-pdf.php
+++ gerador-pdf.php
@@ -1,11 +1,18 @@
 <?php
 
+require "vendor/autoload.php";
+
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
+
+ob_start();
+require "conteudo-pdf.php";
+$html = ob_get_clean();
+
+$dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
 $dompdf->setPaper('A4', 'landscape');
